a8f928fc662fee4c40b573dfcf9e6364c17719e4
Zarnescu Raul <zarnescuraul@gmail.com>
2025-12-07 20:25:33 +0200

    Revert "Integrarea meniului in baza de date"
    This reverts commit 7791bb7b1d132a0e7293bc9bcc9c7aa45e22277c.

--- views/pages/menu.php
+++ views/pages/menu.php
@@ -1,39 +1,4 @@
-<?php
-// Verify database connection is available
-if (!isset($conn)) {
-    require_once __DIR__ . '/../../config/db.php';
-}
-
-// Fetch products
-$sql = "SELECT * FROM products WHERE category = 'coffee' ORDER BY name ASC";
-$result = $conn->query($sql);
-?>
-<link rel="stylesheet" href="assets/css/menu.css">
-
-<section class="menu-section">
-    <div class="menu-container">
-        <h2 class="section-title">Our Coffee Selection</h2>
-        
-        <?php if ($result && $result->num_rows > 0): ?>
-            <div class="menu-grid">
-                <?php while($row = $result->fetch_assoc()): ?>
-                    <div class="product-card">
-                        <div class="product-image">
-                            <!-- Use relative path from root is assumed, or ensure path from DB is correct -->
-                            <img src="<?= htmlspecialchars($row['image_path']) ?>" alt="<?= htmlspecialchars($row['name']) ?>" loading="lazy">
-                        </div>
-                        <div class="product-info">
-                            <h3 class="product-name"><?= htmlspecialchars($row['name']) ?></h3>
-                            <p class="product-description"><?= htmlspecialchars($row['description']) ?></p>
-                            <div class="product-price"><?= number_format($row['price'], 2) ?> RON</div>
-                        </div>
-                    </div>
-                <?php endwhile; ?>
-            </div>
-        <?php else: ?>
-            <div class="no-products">
-                <p>We are currently updating our menu. Please check back soon!</p>
-            </div>
-        <?php endif; ?>
-    </div>
-</section>
+<h2>This is the Menu page</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam expedita fugit, eveniet consequuntur, ducimus distinctio voluptatem ratione a qui esse enim ipsa similique vel minus delectus. Neque corrupti animi voluptatum?</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam expedita fugit, eveniet consequuntur, ducimus distinctio voluptatem ratione a qui esse enim ipsa similique vel minus delectus. Neque corrupti animi voluptatum?</p>
+                
