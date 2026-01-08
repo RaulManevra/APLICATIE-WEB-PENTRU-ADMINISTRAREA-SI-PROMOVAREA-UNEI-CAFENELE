9bcaf5cba511c2cbd681c6b6a6814b1b6f2bfcbe
Zarnescu Raul <zarnescuraul@gmail.com>
2026-01-08 19:53:41 +0200

    Update Baza de date
    Am uitat sa o fac la push-ul trecut

--- Baza de date/_backups/backup_mazi_coffee_08-01-Thu_2026-19-53.sql
+++ Baza de date/_backups/backup_mazi_coffee_08-01-Thu_2026-19-53.sql
@@ -36,7 +36,7 @@ CREATE TABLE `admin_notes` (
 
 LOCK TABLES `admin_notes` WRITE;
 /*!40000 ALTER TABLE `admin_notes` DISABLE KEYS */;
-INSERT INTO `admin_notes` VALUES (1,'Salut\r\n','2026-01-08 14:44:02');
+INSERT INTO `admin_notes` VALUES (1,'','2026-01-08 09:21:46');
 /*!40000 ALTER TABLE `admin_notes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,7 +82,7 @@ CREATE TABLE `order_items` (
   KEY `product_id` (`product_id`),
   CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE,
   CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,7 +91,7 @@ CREATE TABLE `order_items` (
 
 LOCK TABLES `order_items` WRITE;
 /*!40000 ALTER TABLE `order_items` DISABLE KEYS */;
-INSERT INTO `order_items` VALUES (1,1,1,1,13.00),(2,1,2,1,15.00),(3,2,15,4,7.00),(4,2,7,3,5.00),(5,3,2,1,15.00),(6,3,10,3,15.00),(7,3,7,2,5.00),(8,4,11,2,7.00),(9,4,11,1,7.00),(10,5,5,1,14.00),(11,6,2,2,15.00),(12,7,1,2,13.00),(13,7,6,2,6700.00),(14,7,13,2,7.00),(15,8,9,5,15.00),(16,9,1,1,13.00),(17,10,4,5,12.00),(18,10,1,2,13.00),(19,11,11,3,7.00),(20,11,11,1,7.00),(21,11,7,4,5.00),(22,12,11,1,7.00),(23,12,15,4,7.00),(24,13,12,4,7.00),(25,13,6,5,6700.00),(26,14,15,1,7.00),(27,15,14,1,7.00),(28,16,7,2,5.00),(29,16,9,4,15.00),(30,17,6,2,6700.00),(31,18,12,5,7.00),(32,18,6,2,6700.00),(33,18,10,2,15.00),(34,19,14,1,7.00),(35,19,11,5,7.00),(36,19,9,5,15.00),(37,20,7,3,5.00),(38,20,7,5,5.00),(39,21,10,4,15.00),(40,21,14,5,7.00),(41,21,13,3,7.00),(42,22,7,4,5.00),(43,22,9,5,15.00),(44,22,4,1,12.00),(45,23,2,4,15.00),(46,23,3,3,10.00),(47,24,13,2,7.00),(48,25,14,2,7.00),(49,26,11,1,7.00),(50,26,14,5,7.00),(51,27,5,5,14.00),(52,28,9,2,15.00),(53,28,12,4,7.00),(54,28,5,2,14.00),(55,29,7,5,5.00),(56,30,9,5,15.00),(57,30,2,2,15.00),(58,30,1,3,13.00),(59,31,12,1,7.00),(60,32,7,1,5.00),(61,33,7,1,5.00),(62,34,7,1,5.00),(63,35,3,1,10.00),(64,36,1,1,13.00);
+INSERT INTO `order_items` VALUES (1,1,1,1,13.00),(2,1,2,1,15.00),(3,2,15,4,7.00),(4,2,7,3,5.00),(5,3,2,1,15.00),(6,3,10,3,15.00),(7,3,7,2,5.00),(8,4,11,2,7.00),(9,4,11,1,7.00),(10,5,5,1,14.00),(11,6,2,2,15.00),(12,7,1,2,13.00),(13,7,6,2,6700.00),(14,7,13,2,7.00),(15,8,9,5,15.00),(16,9,1,1,13.00),(17,10,4,5,12.00),(18,10,1,2,13.00),(19,11,11,3,7.00),(20,11,11,1,7.00),(21,11,7,4,5.00),(22,12,11,1,7.00),(23,12,15,4,7.00),(24,13,12,4,7.00),(25,13,6,5,6700.00),(26,14,15,1,7.00),(27,15,14,1,7.00),(28,16,7,2,5.00),(29,16,9,4,15.00),(30,17,6,2,6700.00),(31,18,12,5,7.00),(32,18,6,2,6700.00),(33,18,10,2,15.00),(34,19,14,1,7.00),(35,19,11,5,7.00),(36,19,9,5,15.00),(37,20,7,3,5.00),(38,20,7,5,5.00),(39,21,10,4,15.00),(40,21,14,5,7.00),(41,21,13,3,7.00),(42,22,7,4,5.00),(43,22,9,5,15.00),(44,22,4,1,12.00),(45,23,2,4,15.00),(46,23,3,3,10.00),(47,24,13,2,7.00),(48,25,14,2,7.00),(49,26,11,1,7.00),(50,26,14,5,7.00),(51,27,5,5,14.00),(52,28,9,2,15.00),(53,28,12,4,7.00),(54,28,5,2,14.00),(55,29,7,5,5.00),(56,30,9,5,15.00),(57,30,2,2,15.00),(58,30,1,3,13.00);
 /*!40000 ALTER TABLE `order_items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,15 +106,13 @@ CREATE TABLE `orders` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `pickup_time` datetime NOT NULL,
-  `status` enum('pending','preparing','ready','completed','cancelled') COLLATE utf8mb4_unicode_ci DEFAULT 'pending',
+  `status` enum('pending','completed','cancelled') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pending',
   `total_price` decimal(10,2) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `table_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  KEY `table_id` (`table_id`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,7 +121,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,1,'2026-01-09 10:32:00','completed',28.00,'2026-01-08 08:35:15',1),(2,2,'2026-01-08 13:30:11','completed',43.00,'2026-01-08 11:30:11',NULL),(3,2,'2026-01-08 13:30:11','completed',70.00,'2026-01-08 11:30:11',NULL),(4,2,'2026-01-08 13:30:11','completed',21.00,'2026-01-08 11:30:11',NULL),(5,2,'2026-01-08 13:30:11','completed',14.00,'2026-01-08 11:30:11',NULL),(6,2,'2026-01-08 13:30:11','completed',30.00,'2026-01-08 11:30:11',NULL),(7,2,'2026-01-07 13:30:11','completed',13440.00,'2026-01-07 11:30:11',NULL),(8,2,'2026-01-07 13:30:11','completed',75.00,'2026-01-07 11:30:11',NULL),(9,2,'2026-01-07 13:30:11','completed',13.00,'2026-01-07 11:30:11',NULL),(10,2,'2026-01-06 13:30:11','completed',86.00,'2026-01-06 11:30:11',NULL),(11,2,'2026-01-06 13:30:11','completed',48.00,'2026-01-06 11:30:11',NULL),(12,2,'2026-01-06 13:30:11','completed',35.00,'2026-01-06 11:30:11',NULL),(13,2,'2026-01-06 13:30:11','completed',33528.00,'2026-01-06 11:30:11',NULL),(14,2,'2026-01-06 13:30:11','completed',7.00,'2026-01-06 11:30:11',NULL),(15,2,'2026-01-05 13:30:11','completed',7.00,'2026-01-05 11:30:11',NULL),(16,2,'2026-01-05 13:30:11','completed',70.00,'2026-01-05 11:30:11',NULL),(17,2,'2026-01-05 13:30:11','completed',13400.00,'2026-01-05 11:30:11',NULL),(18,2,'2026-01-04 13:30:11','completed',13465.00,'2026-01-04 11:30:11',NULL),(19,2,'2026-01-04 13:30:11','completed',117.00,'2026-01-04 11:30:11',NULL),(20,2,'2026-01-04 13:30:11','completed',40.00,'2026-01-04 11:30:11',NULL),(21,2,'2026-01-04 13:30:11','completed',116.00,'2026-01-04 11:30:11',NULL),(22,2,'2026-01-04 13:30:11','completed',107.00,'2026-01-04 11:30:11',NULL),(23,2,'2026-01-03 13:30:11','completed',90.00,'2026-01-03 11:30:11',NULL),(24,2,'2026-01-03 13:30:11','completed',14.00,'2026-01-03 11:30:11',NULL),(25,2,'2026-01-03 13:30:11','completed',14.00,'2026-01-03 11:30:11',NULL),(26,2,'2026-01-03 13:30:11','completed',42.00,'2026-01-03 11:30:11',NULL),(27,2,'2026-01-03 13:30:11','completed',70.00,'2026-01-03 11:30:11',NULL),(28,2,'2026-01-02 13:30:11','completed',86.00,'2026-01-02 11:30:11',NULL),(29,2,'2026-01-02 13:30:11','completed',25.00,'2026-01-02 11:30:11',NULL),(30,2,'2026-01-02 13:30:11','completed',144.00,'2026-01-02 11:30:11',NULL),(31,1,'2026-01-09 10:56:00','completed',7.00,'2026-01-08 16:57:21',NULL),(32,1,'2026-01-09 10:29:00','completed',5.00,'2026-01-08 17:13:36',NULL),(33,1,'2026-01-08 19:16:37','completed',5.00,'2026-01-08 17:16:37',1),(34,4,'2026-01-08 19:27:04','completed',5.00,'2026-01-08 17:27:05',1),(35,4,'2026-01-08 19:28:11','completed',10.00,'2026-01-08 17:28:11',1),(36,4,'2026-01-08 19:33:19','completed',13.00,'2026-01-08 17:33:19',1);
+INSERT INTO `orders` VALUES (1,1,'2026-01-09 10:32:00','pending',28.00,'2026-01-08 08:35:15'),(2,2,'2026-01-08 13:30:11','completed',43.00,'2026-01-08 11:30:11'),(3,2,'2026-01-08 13:30:11','completed',70.00,'2026-01-08 11:30:11'),(4,2,'2026-01-08 13:30:11','completed',21.00,'2026-01-08 11:30:11'),(5,2,'2026-01-08 13:30:11','completed',14.00,'2026-01-08 11:30:11'),(6,2,'2026-01-08 13:30:11','completed',30.00,'2026-01-08 11:30:11'),(7,2,'2026-01-07 13:30:11','completed',13440.00,'2026-01-07 11:30:11'),(8,2,'2026-01-07 13:30:11','completed',75.00,'2026-01-07 11:30:11'),(9,2,'2026-01-07 13:30:11','completed',13.00,'2026-01-07 11:30:11'),(10,2,'2026-01-06 13:30:11','completed',86.00,'2026-01-06 11:30:11'),(11,2,'2026-01-06 13:30:11','completed',48.00,'2026-01-06 11:30:11'),(12,2,'2026-01-06 13:30:11','completed',35.00,'2026-01-06 11:30:11'),(13,2,'2026-01-06 13:30:11','completed',33528.00,'2026-01-06 11:30:11'),(14,2,'2026-01-06 13:30:11','completed',7.00,'2026-01-06 11:30:11'),(15,2,'2026-01-05 13:30:11','completed',7.00,'2026-01-05 11:30:11'),(16,2,'2026-01-05 13:30:11','completed',70.00,'2026-01-05 11:30:11'),(17,2,'2026-01-05 13:30:11','completed',13400.00,'2026-01-05 11:30:11'),(18,2,'2026-01-04 13:30:11','completed',13465.00,'2026-01-04 11:30:11'),(19,2,'2026-01-04 13:30:11','completed',117.00,'2026-01-04 11:30:11'),(20,2,'2026-01-04 13:30:11','completed',40.00,'2026-01-04 11:30:11'),(21,2,'2026-01-04 13:30:11','completed',116.00,'2026-01-04 11:30:11'),(22,2,'2026-01-04 13:30:11','completed',107.00,'2026-01-04 11:30:11'),(23,2,'2026-01-03 13:30:11','completed',90.00,'2026-01-03 11:30:11'),(24,2,'2026-01-03 13:30:11','completed',14.00,'2026-01-03 11:30:11'),(25,2,'2026-01-03 13:30:11','completed',14.00,'2026-01-03 11:30:11'),(26,2,'2026-01-03 13:30:11','completed',42.00,'2026-01-03 11:30:11'),(27,2,'2026-01-03 13:30:11','completed',70.00,'2026-01-03 11:30:11'),(28,2,'2026-01-02 13:30:11','completed',86.00,'2026-01-02 11:30:11'),(29,2,'2026-01-02 13:30:11','completed',25.00,'2026-01-02 11:30:11'),(30,2,'2026-01-02 13:30:11','completed',144.00,'2026-01-02 11:30:11');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -260,8 +258,8 @@ CREATE TABLE `tables` (
   `x_pos` float DEFAULT '10',
   `y_pos` float DEFAULT '10',
   `shape` enum('circle','square','rectangle') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'circle',
-  `width` float DEFAULT '10',
-  `height` float DEFAULT '10',
+  `width` int DEFAULT '60',
+  `height` int DEFAULT '60',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -272,7 +270,7 @@ CREATE TABLE `tables` (
 
 LOCK TABLES `tables` WRITE;
 /*!40000 ALTER TABLE `tables` DISABLE KEYS */;
-INSERT INTO `tables` VALUES (1,'Libera',27.7638,10.0671,'circle',9.375,12.3296),(2,'Ocupata',43.7186,56.2081,'circle',5.62382,7.4979),(3,'Libera',56.6583,56.2081,'circle',5.62382,7.4979),(4,'Ocupata',69.598,56.2081,'circle',5.62382,7.4979),(5,'Libera',22.2365,40.8233,'square',6.75,7),(6,'Libera',45.9799,76.8456,'rectangle',5.25,14.1667),(7,'Libera',67.2111,77.1812,'rectangle',5.37453,13.6666);
+INSERT INTO `tables` VALUES (1,'Libera',27.925,30.8062,'circle',70,70),(2,'Ocupata',43.3313,64.2844,'circle',45,45),(3,'Rezervata',56.1797,64.3562,'circle',45,45),(4,'Ocupata',68.7891,64.3406,'circle',45,45),(5,'Libera',22.7943,53.0523,'square',54,42),(6,'Libera',45.4864,79.5193,'rectangle',42,85),(7,'Libera',66.1875,79.4989,'rectangle',47,83);
 /*!40000 ALTER TABLE `tables` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -298,7 +296,7 @@ CREATE TABLE `users` (
   `blacklist_reason` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -307,7 +305,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'zarnescuraul@gmail.com','Raul','$2y$12$j09C55tliX7bS9ZCNMLFJuerVS.0DZ0pG/pgqVzLaFcFtYosnA8Iq','admin','2025-11-24 10:19:31',34,'assets/uploads/profile_pictures/PP_userid_1.jpg','google','105941628733277196959',0,NULL),(2,'davidrares56@yahoo.com','raress_tc','$2y$12$ud4pLmRn0Vx6Vm0NpNqXY.nnTbNMkNqKmvsru14zHnrYoXpm/.JHi','admin','2025-12-08 17:24:04',10000,'assets/public/default.png',NULL,NULL,0,NULL),(3,'erwin.georgescu@student.unitbv.ro','erwin','$2y$12$K6Bet6UcpmUmXdC9I4FlYOhtazj9eQf.UeIxyKgoiQ7FuQxmSTmDW','admin','2025-12-27 15:34:58',0,'assets/public/default.png',NULL,NULL,0,NULL),(4,'guest@mazicoffee.com','Guest','$2y$10$ya7vmoIY9nQQDqeBygttKOiTfRSHLBNvxc02utkPBrEUmLzQZIEtK','user','2026-01-08 17:27:05',NULL,'assets/public/default.png',NULL,NULL,0,NULL);
+INSERT INTO `users` VALUES (1,'zarnescuraul@gmail.com','Raul','$2y$12$j09C55tliX7bS9ZCNMLFJuerVS.0DZ0pG/pgqVzLaFcFtYosnA8Iq','admin','2025-11-24 10:19:31',34,'assets/uploads/profile_pictures/PP_userid_1.jpg','google','105941628733277196959',0,NULL),(2,'davidrares56@yahoo.com','raress_tc','$2y$12$ud4pLmRn0Vx6Vm0NpNqXY.nnTbNMkNqKmvsru14zHnrYoXpm/.JHi','admin','2025-12-08 17:24:04',10000,'assets/public/default.png',NULL,NULL,0,NULL),(3,'erwin.georgescu@student.unitbv.ro','erwin','$2y$12$K6Bet6UcpmUmXdC9I4FlYOhtazj9eQf.UeIxyKgoiQ7FuQxmSTmDW','admin','2025-12-27 15:34:58',0,'assets/public/default.png',NULL,NULL,0,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -320,4 +318,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-08 19:53:23
+-- Dump completed on 2026-01-08 13:43:54
